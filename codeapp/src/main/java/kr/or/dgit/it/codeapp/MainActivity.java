c93ba1af9ce780a400a930851996a3b0af367478
EunAeKim <eunae0474@naver.com>
2018-04-17 12:39:12 +0900

    code작성과 xml작성의 차이 비교

---
+++ codeapp/src/main/java/kr/or/dgit/it/codeapp/MainActivity.java
@@ -0,0 +1,36 @@
+package kr.or.dgit.it.codeapp;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setTitle("code로 작성한 액티비티");
+
+        LinearLayout rootLayout = new LinearLayout(this);
+        rootLayout.setOrientation(LinearLayout.VERTICAL);
+
+        Button leftBtn = new Button(this);
+        leftBtn.setText("Left");
+        rootLayout.addView(leftBtn);
+
+        Button rightBtn = new Button(this);
+        rightBtn.setText("Right");
+        rootLayout.addView(rightBtn);
+        rightBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), LayoutActivity.class));
+            }
+        });
+
+        setContentView(rootLayout);
+    }
+}
